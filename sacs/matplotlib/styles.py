f8c633cdc48a2117d0e1ccd3ffaabcef87fca6b2
AntoineSIMTEK <antoine.sanner@imtek.uni-freiburg.de>
2022-01-09 08:53:22 +0100

    MAINT: style for notebook

--- sacs/matplotlib/styles.py
+++ sacs/matplotlib/styles.py
@@ -23,3 +23,29 @@ paper = {
     "savefig.dpi": 300,
     "axes.grid": "False",
     }
+
+notebook = {
+    "font.family": "Arial Unicode MS",
+    "axes.titlesize": "8",
+    "axes.labelsize": 8,
+    "font.size": "8",
+    "legend.fontsize": 7,
+    "lines.linewidth": "1",
+    "lines.markersize": " 4",
+    "xtick.labelsize": " 8",
+    "ytick.labelsize": "8",
+    "axes.formatter.limits": (-3, 4),
+    "figure.figsize": "3.9, 1.5",
+    "figure.titlesize": "small",
+    "figure.subplot.left": " 0.15",  ## the left side of the subplots of the figure"
+    "figure.subplot.right": " 0.95",  ## the right side of the subplots of the figure"
+    "figure.subplot.bottom": " 0.15",  ## the bottom of the subplots of the figure"
+    "figure.subplot.top": "0.95",  ## the top of the subplots of the figure"
+    "figure.subplot.wspace": "0.15",  ## the amount of width reserved for space between subplots,"
+    ## expressed as a fraction of the average axis width
+    "figure.subplot.hspace": " 0.1",  ## the amount of height reserved for space between subplots,"
+    ## expressed as a fraction of the average axis height
+    "figure.dpi": 200,
+    "savefig.dpi": 300,
+    "axes.grid": "False",
+    }
