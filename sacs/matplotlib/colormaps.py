ad9bb7314ecaf1e968a30ed002eb50864bef4f08
AntoineSIMTEK <antoine.sanner@imtek.uni-freiburg.de>
2022-04-01 16:30:58 +0200

    ENH: CMAPs from Paul Tol

--- sacs/matplotlib/colormaps.py
+++ sacs/matplotlib/colormaps.py
@@ -1,6 +1,7 @@
 import matplotlib as mpl
 import numpy as np
 import scipy as sp
+import sacs
 
 
 class MidpointNormalize(mpl.colors.Normalize):
@@ -21,3 +22,29 @@ class MidpointNormalize(mpl.colors.Normalize):
         normalized_mid = 0.5
         return np.interp(value, [normalized_min, normalized_mid,
                                  normalized_max], [self.vmin, self.midpoint, self.vmax])
+
+# https://github.com/Descanonge/tol_colors/blob/5815a3cb74834f70b56e0e131c1ff73bd51e0df4/src/tol_colors/__init__.py#L48-L56
+"""
+Copyright (c) 2019, Paul Tol
+All rights reserved.
+License:  Standard 3-clause BSD
+"""
+
+def discretemap(colormap_name, hexclrs):
+    """Produce a colormap from a list of discrete colors without interpolation."""
+    clrs = to_rgba_array(hexclrs)
+    clrs = np.vstack([clrs[0], clrs, clrs[-1]])
+    cdict = {}
+    for ki, key in enumerate(('red', 'green', 'blue')):
+        cdict[key] = [(i/(len(clrs)-2.), clrs[i, ki], clrs[i+1, ki])
+                      for i in range(len(clrs)-1)]
+    return mpl.colors.LinearSegmentedColormap(colormap_name, cdict)
+
+# How to make a normal colormap
+# cmap = LinearSegmentedColormap.from_list(self.cname, clrs)
+# cmap.set_bad('#999999')
+
+def get_cmap(cmap):
+    c = mpl.colors.LinearSegmentedColormap.from_list(cmap, sacs.matplotlib.colors.__dict__[cmap])
+    c.set_bad(sacs.matplotlib.colors.__dict__[cmap+"_bad_data"])
+    return c
---
+++ test/test_colormaps.py
@@ -0,0 +1,5 @@
+
+from sacs.matplotlib import colormaps as cm
+
+def test_colormaps():
+    cm.get_cmap("sunset")
