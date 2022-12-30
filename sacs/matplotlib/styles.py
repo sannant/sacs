4afa334dbf41e34475cc8687e82323961f3ab149
sannant <antoine.sanner@imtek.uni-freiburg.de>
2022-12-30 10:54:16 +0100

    ENH: style for thesis

--- sacs/matplotlib/styles.py
+++ sacs/matplotlib/styles.py
@@ -72,6 +72,38 @@ paper_nature = {
     "savefig.dpi": 300,
     }
 
+
+thesis = {
+    "font.family": "Arial Unicode MS",
+    "axes.grid": "False",
+    "axes.prop_cycle": default_color_cycler,
+    "axes.titlesize": "10",
+    "axes.labelsize": 10,
+    "axes.xmargin": 0,  # x margin.  See `axes.Axes.margins`
+    "axes.ymargin": 0,  # y margin.  See `axes.Axes.margins`
+    "font.size": "8",
+    "legend.fontsize": 8,
+    "lines.linewidth": "1",
+    "lines.markersize": " 4",
+    "xtick.labelsize": "8",
+    "ytick.labelsize": "8",
+    "axes.formatter.limits": (-3, 4),
+    "figure.figsize": (mm2inch * 140, mm2inch * 60),
+    "figure.titlesize": "small",
+    "figure.subplot.left": " 0.15",  ## the left side of the subplots of the figure"
+    "figure.subplot.right": " 0.95",  ## the right side of the subplots of the figure"
+    "figure.subplot.bottom": " 0.15",  ## the bottom of the subplots of the figure"
+    "figure.subplot.top": "0.95",  ## the top of the subplots of the figure"
+    "figure.subplot.wspace": "0.15",  ## the amount of width reserved for space between subplots,"
+    ## expressed as a fraction of the average axis width
+    "figure.subplot.hspace": " 0.1",  ## the amount of height reserved for space between subplots,"
+    ## expressed as a fraction of the average axis height
+    "figure.dpi": 300,
+    "savefig.dpi": 300,
+    }
+
+
+
 notebook = {
     "font.family": "Arial Unicode MS",
     "axes.grid": "False",
